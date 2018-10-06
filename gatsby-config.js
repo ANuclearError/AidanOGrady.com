61a328e9a1a46f9aa17163ba58ec10ed1e8d41e4
Aidan O'Grady <aidan@aidanogrady.com>
2018-10-06 14:48:34 +0100

    Add siteMetadata to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    siteMetadata: {
+        title: "Aidan O'Grady",
+        siteUrl: "https://www.aidanogrady.com",
+        description: "The personal website of Aidan O'Grady"
+    },
     plugins: [
         {
             resolve: "gatsby-plugin-typography",
